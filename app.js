26b3dd87ce692a2880179cbb167c5fa186c0aaa6
Aluo <green.rabite@gmail.com>
2018-05-06 21:41:18 -0700

    Testing websockets with vanilla js

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 //setup view engine for index.html page
+app.set('view engine', 'ejs');
+
+//public static directory
 app.use(express.static('public'));
+
 
 // Routes
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.render('index');
 });
 
 
 // Instruct node to listen to port 3000
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+let server = app.listen(PORT);
+
+const io = require('socket.io')(server);
+
+
+// Websockets listening to every connection
+io.on('connection',(socket)=>{
+  console.log('New User connected');
+
+  // default username
+  socket.username = "Annoymous";
+
+  // listen on change_username
+  socket.on('change_username',data =>{
+    socket.username = data.username;
+  });
+
+  //listen on new message
+  socket.on('new_message',data=>{
+    // broadcast the new message
+    io.sockets.emit('new_message', {
+      message: data.message,
+      username: socket.username
+    });
+  });
+});
---
+++ public/javascripts/chat.js
@@ -0,0 +1,41 @@
+// make connection
+let socket = io.connect('http://localhost:3000');
+
+// buttons and inputs elements
+let message = document.querySelector('#message');
+let username = document.querySelector('#username');
+let send_message = document.querySelector('#send_message');
+let send_username = document.querySelector('#send_username');
+let chatroom = document.querySelector('#chatroom');
+
+//Emit a username
+function sendUsername(e){
+  if (e.type === 'click' || e.keyCode == 13) {
+    console.log(username.value);
+    socket.emit('change_username',{username: username.value});
+  }
+}
+
+// Emit message
+function sendMessage(e){
+  if (e.type === 'click' || e.keyCode == 13) {
+    socket.emit('new_message',{message: message.value});
+    message.value = "";
+  }
+}
+
+// Listen for new message
+socket.on('new_message',data=>{
+  console.log(data);
+  let p = document.createElement('p');
+  p.classList.add("message");
+  p.textContent = `${data.username}:${data.message}`;
+  chatroom.append(p);
+});
+
+send_message.addEventListener('click', (e)=>sendMessage(e));
+send_message.addEventListener('keydown', (e)=>sendMessage(e));
+message.addEventListener('keydown', (e)=>sendMessage(e));
+send_username.addEventListener('click',(e)=> sendUsername(e));
+send_username.addEventListener('keydown',(e)=> sendUsername(e));
+username.addEventListener('keydown',(e)=>sendUsername(e));
